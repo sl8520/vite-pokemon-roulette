a3ac47e4d732939dac7ac84f67dfa880919b0972
Robin <sl8520@hotmail.com>
2025-05-16 14:55:18 +0800

    feat: change router mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
